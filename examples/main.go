e6cd04e7534e67d80de5b2430d4812b07c1e0322
iqquee <onlyiqquee@gmail.com>
2023-12-12 08:01:51 -0800

    example added for SendBatchTemplatedEmail()

--- examples/main.go
+++ examples/main.go
@@ -81,3 +81,39 @@ func sendTemplatedEmail() {
 		fmt.Printf("response message: %v\n", e.Message)
 	}
 }
+
+// SendBatchTemplatedEmail sends a batch templated email
+func SendBatchTemplatedEmail() {
+	zeptomailToken := "your zeptomail authorization token"
+	tempKey := "your zeptomail template key"
+
+	client := zeptomail.New(*http.DefaultClient, zeptomailToken)
+	sendTo := []zeptomail.SendBatchTemplateEmailTo{}
+	sendTo = append(sendTo, zeptomail.SendBatchTemplateEmailTo{
+		EmailAddress: zeptomail.EmailAddress{
+			Address: "rudra.d@zylker.com",
+			Name:    "Rudra",
+		},
+		MergeInfo: map[string]interface{}{
+			"userID": "my userID",
+		},
+	})
+
+	req := zeptomail.SendBatchTemplatedEmailReq{
+		To: sendTo,
+		From: zeptomail.EmailAddress{
+			Address: "accounts@info.zylker.com",
+			Name:    "Paula",
+		},
+		TemplateKey: tempKey,
+	}
+
+	res, err := client.SendBatchTemplatedEmail(req)
+	if err != nil {
+		fmt.Printf("This is the error: %v", err.Error())
+	}
+
+	for _, e := range res.Data {
+		fmt.Printf("response message: %v\n", e.Message)
+	}
+}
